6869d3538fdb16cffa58c611b9668ed43c5cd9d0
Saravanamoorthy0673 <saravanamoorthy470@gmail.com>
2026-04-20 19:38:11 +0530

    model_download.py

--- model_download.py
+++ model_download.py
@@ -2,7 +2,10 @@ import os
 import requests
 
 def download_model_if_needed():
-    model_path = "skin_disease_model.h5"
+    model_path = os.path.join("model", "skin_disease_model.h5")
+
+    # ✅ ensure folder exists
+    os.makedirs("model", exist_ok=True)
 
     if not os.path.exists(model_path):
         print("Downloading model...")
